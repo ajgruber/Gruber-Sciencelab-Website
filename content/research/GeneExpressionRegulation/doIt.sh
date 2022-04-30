427591a9e27c8ae4d98a133ed6a4531ca4bce998
Andreas Gruber <aj.gruber@unibas.ch>
2022-04-30 17:25:21 +0200

    Add icons

--- content/research/GeneExpressionRegulation/doIt.sh
+++ content/research/GeneExpressionRegulation/doIt.sh
@@ -1,6 +1,6 @@
 # convert the pdf to a png
 convert -density 150 \
-DataScienceTools.pdf \
+/home/ajgruber/Documents/Website/GSLwebsite/Materials/ResearchImages/DataScienceTools/DataScienceTools.pdf \
 -quality 100 \
-DataScienceTools.png
+featured.png
 

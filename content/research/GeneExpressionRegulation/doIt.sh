4b1926adf393cfa5f0fb839ca88625ae9d7e8cab
Andreas Gruber <aj.gruber@unibas.ch>
2022-09-09 09:50:05 +0200

    Removed Gene Expression Regulation.

--- content/research/GeneExpressionRegulation/doIt.sh
+++
@@ -1,6 +0,0 @@
-# convert the pdf to a png
-convert -density 150 \
-/home/ajgruber/Documents/Website/GSLwebsite/Materials/ResearchImages/DataScienceTools/DataScienceTools.pdf \
--quality 100 \
-featured.png
-
